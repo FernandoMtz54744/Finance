8fff2c990f81e6e95e4a0c512ae101fdc09790a3
Fernando <fer_f@outlook.com>
2026-03-26 22:36:21 -0600

    Opcion de sumar temporalmente

--- src/pages/movimientos/MovimientosList.tsx
+++ src/pages/movimientos/MovimientosList.tsx
@@ -1,5 +1,7 @@
 import type { Movimiento } from "@/types/movimiento"
 import MovimientosTable from "./MovimientosTable";
+import { useState } from "react";
+import { formatMXN } from "@/lib/utils";
 
 type Props = {
     movimientos: Movimiento[]
@@ -10,16 +12,22 @@ export default function MovimientosList({movimientos}: Props) {
   const abonos = movimientos.filter(movimiento => ['a', 'r'].includes(movimiento.tipo));
   const cargos = movimientos.filter(movimiento => movimiento.tipo === 'c');
   
+  const [sumaTemp, setSumaTemp] = useState<number>(0);
 
   return (
+    <>
+    {sumaTemp !== 0 && (
+      <div className="text-center text-2xl mt-4 -mb-6">Suma temporal: {formatMXN(sumaTemp)}</div>
+    )}
     <div className="grid grid-cols-12 px-8 md:gap-x-16 gap-y-16 my-8">
         <div className="col-span-12 md:col-span-6">
-           <MovimientosTable movimientos={abonos} title="Abonos" bgColor="bg-emerald-900"/>
+           <MovimientosTable movimientos={abonos} title="Abonos" bgColor="bg-emerald-900" setSumaTemp={setSumaTemp}/>
         </div>
 
         <div className="col-span-12 md:col-span-6">
-            <MovimientosTable movimientos={cargos} title="Cargos" bgColor="bg-red-900"/>
+            <MovimientosTable movimientos={cargos} title="Cargos" bgColor="bg-red-900" setSumaTemp={setSumaTemp}/>
         </div>
     </div>
+    </>
   )
 }
--- src/pages/movimientos/MovimientosTable.tsx
+++ src/pages/movimientos/MovimientosTable.tsx
@@ -5,14 +5,16 @@ import type { Movimiento } from "@/types/movimiento"
 import { useMutation, useQueryClient } from "@tanstack/react-query"
 import { BanknoteArrowUp, Trash2 } from "lucide-react"
 import ErrorPage from "../layouts/ErrorPage"
+import { ContextMenu, ContextMenuContent, ContextMenuItem, ContextMenuTrigger } from "@/components/ui/context-menu"
 
 type Props = {
     movimientos: Movimiento[],
     title: string,
-    bgColor: string
+    bgColor: string,
+    setSumaTemp: React.Dispatch<React.SetStateAction<number>>
 }
 
-export default function MovimientosTable({movimientos, title, bgColor}: Props) {
+export default function MovimientosTable({movimientos, title, bgColor, setSumaTemp}: Props) {
 
   const queryClient = useQueryClient();
   const periodo = usePeriodoStore((state) => state.periodo);
@@ -32,15 +34,29 @@ export default function MovimientosTable({movimientos, title, bgColor}: Props) {
     <>
     <div className="text-center text-2xl font-medium my-2">{title}</div>
     {movimientos.sort((a,b) => IsoToDate(a.fecha).getTime() - IsoToDate(b.fecha).getTime()).map((movimiento, i) => (
-        <div className="flex flex-row justify-between items-center border-b-2 border-b-gray-850 px-4 py-2" key={i}>
-            <div>{dateToString(IsoToDate(movimiento.fecha))}</div>
-            <div className="text-left w-full pl-4 flex flex-row">
-              {movimiento.motivo}
-              {movimiento.tipo === "r" && <BanknoteArrowUp className="text-emerald-400 ml-2"/>}
-            </div>
-            <div>{formatMXN(movimiento.cantidad)}</div>
-            {!periodo.validado && <Trash2 className="ml-2 hover:cursor-pointer hover:text-red-500" onClick={()=>deleteMovimientos.mutate(movimiento.id)}/>}
-        </div>
+      <ContextMenu>
+        <ContextMenuTrigger>
+          <div className="flex flex-row justify-between items-center border-b-2 border-b-gray-850 px-4 py-2" key={i}>
+              <div>{dateToString(IsoToDate(movimiento.fecha))}</div>
+              <div className="text-left w-full pl-4 flex flex-row">
+                {movimiento.motivo}
+                {movimiento.tipo === "r" && <BanknoteArrowUp className="text-emerald-400 ml-2"/>}
+              </div>
+              <div>{formatMXN(movimiento.cantidad)}</div>
+              {!periodo.validado && <Trash2 className="ml-2 hover:cursor-pointer hover:text-red-500" onClick={()=>deleteMovimientos.mutate(movimiento.id)}/>}
+          </div>
+        </ContextMenuTrigger>
+        <ContextMenuContent>
+            <ContextMenuItem className="hover:cursor-pointer"
+                onClick={()=>setSumaTemp(suma => suma + movimiento.cantidad)}>
+                Agregar a suma
+            </ContextMenuItem>
+            <ContextMenuItem className="hover:cursor-pointer"
+                onClick={()=>setSumaTemp(suma => suma - movimiento.cantidad)}>
+                Eliminar de suma
+            </ContextMenuItem>
+        </ContextMenuContent>
+      </ContextMenu>
     ))}
     <div className={cn("flex flex-row justify-between mt-8 rounded-md py-2 px-4", bgColor)}>
       <div>Total:</div>
