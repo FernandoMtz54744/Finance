9779de776b964d2cb6f65830f13cb180d72b32d9
Fernando <fer_f@outlook.com>
2026-04-03 22:44:33 -0600

    Mobile responsive Movimientos

--- src/pages/movimientos/MovimientosList.tsx
+++ src/pages/movimientos/MovimientosList.tsx
@@ -11,6 +11,7 @@ export default function MovimientosList({movimientos}: Props) {
    
   const abonos = movimientos.filter(movimiento => movimiento.cantidad > 0);
   const cargos = movimientos.filter(movimiento => movimiento.cantidad < 0);
+  const [tipoMovil, setTipoMovil] = useState<"abonos" | "cargos">("abonos");
   
   const [sumaTemp, setSumaTemp] = useState<number>(0);
 
@@ -19,15 +20,34 @@ export default function MovimientosList({movimientos}: Props) {
     {sumaTemp !== 0 && (
       <div className="text-center text-2xl mt-4 -mb-6">Suma temporal: {formatMXN(sumaTemp)}</div>
     )}
-    <div className="grid grid-cols-12 px-8 md:gap-x-16 gap-y-16 my-8">
-        <div className="col-span-12 md:col-span-6">
+    <div className="hidden md:grid grid-cols-12 px-8 gap-x-16 gap-y-16 my-8">
+        <div className="col-span-6">
            <MovimientosTable movimientos={abonos} title="Abonos" bgColor="bg-emerald-900" setSumaTemp={setSumaTemp}/>
         </div>
 
-        <div className="col-span-12 md:col-span-6">
+        <div className="col-span-6">
             <MovimientosTable movimientos={cargos} title="Cargos" bgColor="bg-red-900" setSumaTemp={setSumaTemp}/>
         </div>
     </div>
-    </>
-  )
-}
+    {/* Mobile */}
+    <div className="md:hidden px-4 mt-6">
+      <div className="flex justify-center gap-4 mb-4">
+        <button onClick={() => setTipoMovil("abonos")} 
+          className={`px-4 py-2 rounded bg-emerald-900  ${tipoMovil !== "abonos" && "opacity-50"}`}>
+            Abonos
+        </button>
+
+        <button onClick={() => setTipoMovil("cargos")}
+          className={`px-4 py-2 rounded bg-red-900 ${tipoMovil !== "cargos" && "opacity-50"}`}>
+            Cargos
+        </button>
+      </div>
+
+      <MovimientosTable movimientos={tipoMovil === "abonos" ? abonos : cargos}
+          title={tipoMovil === "abonos" ? "Abonos" : "Cargos"}
+          bgColor={tipoMovil === "abonos" ? "bg-emerald-900" : "bg-red-900"}
+          setSumaTemp={setSumaTemp}
+        />
+    </div>
+  </>
+)}
